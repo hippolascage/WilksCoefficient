68a020c0058764f702e43c4fb4c987b001575141
Sunny Wijeratne <sunny.vigilante@gmail.com>
2015-09-07 18:52:41 +1000

    more files

---
+++ WilksCoefficient/Coefficients.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilksCoefficient
+{
+    public class Coefficients
+    {
+        public double a;
+        public double b;
+        public double c;
+        public double d;
+        public double e;
+        public double f;
+
+        public Coefficients(bool isMale)
+        {
+            if (isMale)
+            {
+                a = -216.0475144;
+                b = 16.2606339;
+                c = -0.002388645;
+                d = -0.00113732;
+                e = 7.01863E-06;
+                f = -1.291E-08;
+            }
+            else
+            {
+                a = 594.31747775582;
+                b = -27.23842536447;
+                c = 0.82112226871;
+                d = -0.00930733913;
+                e = 0.00004731582;
+                f = -0.00000009054;
+            }
+        }
+
+    }
+}
+
---
+++ WilksCoefficientTests/TestCoefficientInit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WilksCoefficient.Tests
+{
+    [TestClass]
+    public class TestCoefficientInit
+    {
+        [TestMethod]
+        public void TestCoefficientInitMale()
+        {
+            double expected_male_a = -216.0475144;
+
+            var maleCoefficients = new Coefficients(true);
+            double actual_male_a = maleCoefficients.a;
+
+            Assert.AreEqual(expected_male_a, actual_male_a);
+
+        }
+        [TestMethod]
+        public void TestCoefficientInitFemale()
+        {
+            double expected_female_a = 594.31747775582;
+
+            var femaleCoefficients = new Coefficients(false);
+            double actual_female_a = femaleCoefficients.a;
+
+            Assert.AreEqual(expected_female_a, actual_female_a);
+        }
+    }
+}
